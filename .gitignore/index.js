594d1eda3f772bbe4f31ea1d9a52c70a30c3aa47
Azogs <47117345+Azogs@users.noreply.github.com>
2019-01-28 19:29:56 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,7 +5,7 @@ var PREFIX = "!";
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("TutoBot, !help");
+    bot.user.setGame("ZogZog, !help");
     console.log("Le bot a bien ete connecte");
 });
 
